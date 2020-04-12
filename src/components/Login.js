5000ab654d1c915364d5060a2e71a7fba0158eb6
Tony <tony.krikorian@outlook.com>
2020-04-12 15:54:02 -0400

    Center login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import logo from "../logo.svg";
 
 class Login extends Component {
   state = {};
   render() {
     return (
-      <div>
-        <h2>Login</h2>
+      <div className="row">
+        <div className="col-md"></div>
+        <div className="col-md">
+          <div className="card">
+            <div className="card-body">
+              <div className="card-title">
+                <div class="mx-auto text-center">
+                  <h5>Login</h5>
+                </div>
+              </div>
+              <img src={logo} />
+            </div>
+          </div>
+        </div>
+        <div className="col-md"></div>
       </div>
     );
   }

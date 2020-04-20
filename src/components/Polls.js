709d56914a0c8e54961a5c8512192279f76fd438
Tony <tony.krikorian@outlook.com>
2020-04-20 18:46:54 -0400

    Remove unnecessary code

--- src/components/Polls.js
+++ src/components/Polls.js
@@ -1,8 +1,6 @@
 import React, { Component, Fragment } from "react";
 import Tabs from "react-bootstrap/Tabs";
 import Tab from "react-bootstrap/Tab";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import { connect } from "react-redux";
 import CardPoll from "./CardPoll";
 class Polls extends Component {

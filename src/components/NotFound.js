cef6765fa6b325ebe9d7f082d3f58cd66a5f9409
Tony <tony.krikorian@outlook.com>
2020-04-21 19:45:43 -0400

    Set Unanswered questions by default

---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const NotFound = () => {
+  return (
+    <Fragment>
+      <div className="row mx-auto">
+        <img src="/404.jpg" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default NotFound;
